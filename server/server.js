155cce78316cffb7bc31160bb1a00cb625cd5943
NIHAL SHEIKH <130501159+MuhammadNihalImran@users.noreply.github.com>
2024-08-27 12:42:32 +0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const path = require("path"); // Require the path module
 const cors = require("cors");
 app.use(
   cors({
-    origin: ["https://deploy-mern-1whq.vercel.app"],
+    origin: ["https://image-upload-zxbq.vercel.app"],
     methods: ["POST", "GET"],
     credentials: true,
   })
